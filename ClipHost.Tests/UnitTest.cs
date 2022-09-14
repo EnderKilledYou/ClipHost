d78722a19b71778a9e550bc21350e1c76fc9d266
Ender <sterlingkooshesh@gmail.com>
2022-09-13 19:27:05 -0500

    a

--- ClipHost.Tests/UnitTest.cs
+++ ClipHost.Tests/UnitTest.cs
@@ -23,10 +23,6 @@ public class UnitTest
     [Test]
     public void Can_call_MyServices()
     {
-        var service = appHost.Container.Resolve<MyServices>();
 
-        var response = (HelloResponse)service.Any(new Hello { Name = "World" });
-
-        Assert.That(response.Result, Is.EqualTo("Hello, World!"));
     }
 }
